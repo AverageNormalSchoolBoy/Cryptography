dbae17277e6c1270a829674dca6e7392be02ce28
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-06 17:38:29 -0500

    Updated from Brython Server: 2/6/2017 5:38:28 PM

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,25 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+from ggame import App
+myapp.listenKeyEvent('keydown', 'e', eKey)
+myapp.listenKeyEvent('keydown', 'd', dKey)
+myapp.listenKeyEvent('keydown', 'q', qKey)
+
+encrypt == False
+def eKey(event):
+    encrypt == True
+
+if encrypt == True:
+    print("it worked")
+
+    
+
+
+
+
+
+
+
+myapp = App()
+myapp.run()
