f2e980b53fe678ee1761b9e399f1a13790f6599f
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-06 22:14:50 -0500

    Updated from Brython Server: 2/6/2017 10:14:49 PM

--- cryptography.py
+++ cryptography.py
@@ -9,18 +9,42 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+
+
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-from ggame import App
-myapp.listenKeyEvent('keydown', 'e', eKey)
-myapp.listenKeyEvent('keydown', 'd', dKey)
-myapp.listenKeyEvent('keydown', 'q', qKey)
 
-encrypt == False
-def eKey(event):
-    encrypt == True
+input1 = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if input1 == 'e':
+    message = input('Message: ')
+    key = input('Key: ')
+elif input1 == 'd'
+    message = input('Message: ')
+    key = input('Key: ')
+elif input1 == 'q'
+    print('Goodbye! ')
+else: 
+    print('Did not understand command, try again.')
+message = input('Message: ')
+key = input('Key: ')
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-if encrypt == True:
-    print("it worked")
+message = list(message)
+message = [associations.find(x) for x in message]
+key = list(key)
+key = [associations.find(x) for x in key]
+key = [int(x) for x in key]
+message = [int(x) for x in message]
+i=0
+while i < len(message)/len(key):
+    nkey=key.extend(key)
+    i+1
+print(nkey)
+    
+enc = [x+y for x,y in zip(message, key)]
+print(enc)
+
+
+    
 
     
 
@@ -29,6 +53,3 @@ if encrypt == True:
 
 
 
-
-myapp = App()
-myapp.run()
