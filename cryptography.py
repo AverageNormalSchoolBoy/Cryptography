47b5212d11fab3a69a6e46461b96540c2321bee4
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-07 12:30:36 -0500

    Updated from Brython Server: 2/7/2017 12:30:36 PM

--- cryptography.py
+++ cryptography.py
@@ -17,16 +17,13 @@ input1 = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 if input1 == 'e':
     message = input('Message: ')
     key = input('Key: ')
-elif input1 == 'd'
+elif input1 == 'd':
     message = input('Message: ')
     key = input('Key: ')
-elif input1 == 'q'
+elif input1 == 'q':
     print('Goodbye! ')
 else: 
     print('Did not understand command, try again.')
-message = input('Message: ')
-key = input('Key: ')
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
 message = list(message)
 message = [associations.find(x) for x in message]
@@ -34,14 +31,15 @@ key = list(key)
 key = [associations.find(x) for x in key]
 key = [int(x) for x in key]
 message = [int(x) for x in message]
+enc = message
 i=0
-while i < len(message)/len(key):
-    nkey=key.extend(key)
+j=0
+while i < len(message):
+    enc = message[i] + key[j]
     i+1
-print(nkey)
-    
-enc = [x+y for x,y in zip(message, key)]
-print(enc)
+    j+1
+
+
 
 
     
